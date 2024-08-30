02b4747141e5f36d7d849eb166d53f5c5078f0dd
ZhangMeng <n030236@archermind.com>
2024-08-30 14:43:26 +0800

    fix:修复音频播放、录制相关状态问题

--- harmony/audio_toolkit/src/main/ets/RNCAudioPlayerTurboModule.ts
+++ harmony/audio_toolkit/src/main/ets/RNCAudioPlayerTurboModule.ts
@@ -36,7 +36,7 @@ interface PlayConfig {
   looping?: boolean;
   speed?: number;
   autoDestroy?: boolean;
-  continueToPlayInBackground?: boolean;
+  continuesToPlayInBackground?: boolean;
 }
 
 interface PlayInfo {
@@ -45,7 +45,7 @@ interface PlayInfo {
 }
 
 interface Error {
-  err: string;
+  err?: string;
   message?: string;
 }
 
@@ -143,10 +143,13 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
 
   pauseOnBackground(): void {
     this.playConfigMap.forEach((config, playerId) => {
-      if (!config.continueToPlayInBackground) {
+      if (!config.continuesToPlayInBackground) {
         const player = this.playerMap.get(playerId);
         if (player) {
           player.pause();
+          this.toEmit(playerId, 'pause', {
+            message: 'Playback paused due toBackground',
+          });
         }
       }
     });
@@ -163,7 +166,7 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
     });
   }
 
-  setAVPlayerCallback(avPlayer: media.AVPlayer, playerId: number): void {
+  setAVPlayerCallback(avPlayer: media.AVPlayer, playerId: number, next: (object?) => void): void {
     // seek操作结果回调函数
     avPlayer.on('seekDone', (seekDoneTime: number) => {
       logger.debug(`AVPlayer seek succeeded, seek time is ${seekDoneTime}`);
@@ -178,7 +181,7 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
       avPlayer.reset(); // 调用reset重置资源，触发idle状态
     });
     // 状态机变化回调函数
-    this.avPlayerStateChangeCallback(avPlayer, playerId);
+    this.avPlayerStateChangeCallback(avPlayer, playerId, next);
     avPlayer.on('seekDone', () => {
       const call = this.playSeekCallbacks.get(playerId);
       if (call) {
@@ -229,7 +232,7 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
     });
   }
 
-  avPlayerStateChangeCallback(avPlayer: media.AVPlayer, playerId: number): void {
+  avPlayerStateChangeCallback(avPlayer: media.AVPlayer, playerId: number, next: (object?) => void): void {
     avPlayer.on('stateChange', async (state: string, reason: media.StateChangeReason) => {
       logger.debug(`stateChange:${state}`);
       switch (state) {
@@ -242,6 +245,7 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
         case StateChange.PREPARED: // prepare调用成功后上报该状态机
           logger.debug(`prepared called.to and apply config and play`);
           this.applyConfig(playerId);
+          next(null);
           break;
         case StateChange.PLAYING: // play成功调用后触发该状态机上报
           break;
@@ -272,11 +276,11 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
     });
   }
 
-  async createPlayer(pathStr: string, playerId: number): Promise<void> {
+  async createPlayer(pathStr: string, playerId: number, next: (object?) => void): Promise<void> {
     logger.debug(`createPlayer path:${pathStr}`);
     try {
       const avPlayer: media.AVPlayer = await media.createAVPlayer();
-      this.setAVPlayerCallback(avPlayer, playerId);
+      this.setAVPlayerCallback(avPlayer, playerId, next);
       if (pathStr.startsWith(this.HTTP_START)) {
         avPlayer.url = pathStr;
       } else {
@@ -312,25 +316,29 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
     }
   }
 
-  prepare(playerId: number, path: string, option: PlayConfig, next: () => void): void {
+  prepare(playerId: number, path: string, option: PlayConfig, next: (object?) => void): void {
     logger.debug(`prepare start`);
     this.setConfig(playerId, option);
-    this.createPlayer(path, playerId).then(() => {
-      next();
+    this.createPlayer(path, playerId, next).then(() => {
     });
   }
 
-  checkPlayer(playerId: number, next: (err?: string) => void): boolean {
+  checkPlayer(playerId: number, next: (object?) => void): boolean {
     const hasPlayer = this.playerMap.has(playerId);
     if (hasPlayer) {
       return true;
     } else {
-      next?.('not found player');
+      logger.debug('not found player')
+      let err: Error = {
+        err: 'not found player',
+        message: 'not found media player',
+      };
+      next?.(err);
       return false;
     }
   }
 
-  async play(playerId: number, callback: (err: string, result?: PlayInfo) => void): Promise<void> {
+  async play(playerId: number, callback: (object?, result?: PlayInfo) => void): Promise<void> {
     try {
       logger.debug(`play start`);
       if (!this.checkPlayer(playerId, callback)) {
@@ -338,13 +346,17 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
       }
       const player = this.getPlayer(playerId);
       await player.play();
-      callback('', this.getInfo(playerId));
+      callback(null, this.getInfo(playerId));
     } catch (e) {
-      callback?.(`player call play function err:${JSON.stringify(e)}`);
+      let err: Error = {
+        err: 'player function err',
+        message: `player call play function err:${JSON.stringify(e)}`,
+      };
+      callback?.(err, this.getInfo(playerId));
     }
   }
 
-  async pause(playerId: number, callback: (err: string, result?: PlayInfo) => void): Promise<void> {
+  async pause(playerId: number, callback: (object?, result?: PlayInfo) => void): Promise<void> {
     logger.debug(`pause start`);
     if (!this.checkPlayer(playerId, callback)) {
       return;
@@ -354,7 +366,7 @@ export class RCTAudioPlayerTurboModule extends TurboModule {
     this.toEmit(playerId, 'pause', {
       message: 'player paused',
     });
-    callback('', this.getInfo(playerId));
+    callback(null, this.getInfo(playerId));
   }
 
   async stop(playerId: number, callback: () => void): Promise<void> {
